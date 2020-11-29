ee791bc8c6e5e4435da8cc1d7116c0b728d99ec1
Logan Moseley <git@loganmoseley.com>
2020-11-29 10:51:25 -0500

    Copy in Apple's fun touch tracking example

--- Small Games/ContentView.swift
+++ Small Games/ContentView.swift
@@ -5,6 +5,7 @@ import SwiftUI
 class SpellingBeeScene: SKScene {
 
     private var label: SKLabelNode!
+    private var spinnyNode : SKShapeNode?
 
     /// The scene initializer and the color-node child need to have
     /// the same size, but I'm not sure how to *derive* the size,
@@ -23,11 +24,64 @@ class SpellingBeeScene: SKScene {
         super.didMove(to: view)
 
         addChild(SKSpriteNode(
-            color: UIColor.blue,
+            color: UIColor.systemIndigo,
             size: assumedScreenSize))
 
         label = SKLabelNode(text: "Hello, world! \(view.bounds.size)")
         addChild(label)
+
+        // Create shape node to use during mouse interaction
+        let w = (size.width + size.height) * 0.05
+        spinnyNode = SKShapeNode(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+
+        if let spinnyNode = spinnyNode {
+            spinnyNode.lineWidth = 2.5
+
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+                                              SKAction.fadeOut(withDuration: 0.5),
+                                              SKAction.removeFromParent()]))
+        }
+    }
+
+    func touchDown(atPoint pos: CGPoint) {
+        if let n = spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            addChild(n)
+        }
+    }
+
+    func touchMoved(toPoint pos: CGPoint) {
+        if let n = spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            addChild(n)
+        }
+    }
+
+    func touchUp(atPoint pos: CGPoint) {
+        if let n = spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            addChild(n)
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchDown(atPoint: t.location(in: self)) }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchMoved(toPoint: t.location(in: self)) }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { touchUp(atPoint: t.location(in: self)) }
     }
 }
 
